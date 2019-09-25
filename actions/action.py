db7ced2537272d4f8dce96a2f92055692d6b72ca
Jimting <handsome841206@gmail.com>
2019-09-25 10:26:07 +0800

    default actions

--- actions/action.py
+++
@@ -1,7 +0,0 @@
-class ActionDefaultFallback(Action):
-
-   def name(self):
-      return "action_default_fallback"
-
-   def run(self, dispatcher, tracker, domain):
-      dispatcher.utter_message("Sorry, I couldn't understand.")
