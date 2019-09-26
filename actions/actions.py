30c0e43d926604211a6ddd3154567234269d78f6
Jimting <handsome841206@gmail.com>
2019-09-26 17:13:26 +0800

    bots for test...

--- actions/actions.py
+++ actions/actions.py
@@ -1,31 +1,15 @@
-from rasa_sdk import Action
-from rasa_sdk.events import SlotSet
+from rasa_core_sdk import Action
 
+class ActionWeather(Action):
 
-class RestaurantAPI(object):
-    def search(self, info):
-        return "papi's pizza place"
-
-
-class ActionSearchRestaurants(Action):
     def name(self):
-        return "action_search_restaurants"
+        return "action_weather"
 
     def run(self, dispatcher, tracker, domain):
-        dispatcher.utter_message("looking for restaurants")
-        restaurant_api = RestaurantAPI()
-        restaurants = restaurant_api.search(tracker.get_slot("cuisine"))
-        return [SlotSet("matches", restaurants)]
-
-
-class ActionSuggest(Action):
-    def name(self):
-        return "action_suggest"
-
-    def run(self, dispatcher, tracker, domain):
-        dispatcher.utter_message("here's what I found:")
-        dispatcher.utter_message(tracker.get_slot("matches"))
-        dispatcher.utter_message(
-            "is it ok for you? hint: I'm not going to find anything else :)"
-        )
+        where = next(tracker.get_latest_entity_values('weather'), None)
+        if where is not None:
+            dispatcher.utter_message("You asked about {}".format(where))
+        else:
+            dispatcher.utter_message("I couldn't tell where!")
+        
         return []
