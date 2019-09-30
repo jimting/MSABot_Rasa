2c8d970e4fe54e990cd107454571648b960bef1e
Jimting <handsome841206@gmail.com>
2019-09-30 15:52:07 +0800

    Update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -2,68 +2,68 @@ from rasa_core_sdk import Action
 
 class ActionHealth(Action):
 
-    def name(self):
-        return "action_service_health"
+	def name(self):
+		return "action_service_health"
 
-    def run(self, dispatcher, tracker, domain):
-        service = next(tracker.get_latest_entity_values('service'), None)
+	def run(self, dispatcher, tracker, domain):
+		service = next(tracker.get_latest_entity_values('service'), None)
 		searching_service = "none"
-        if service is not None:
-            searching_service = service
+		if service is not None:
+			searching_service = service
 		data = {
 			"intent" : "action_service_health",
 			"service" : searching_service
 		}
-        dispatcher.utter_message(data)
-        return []
+		dispatcher.utter_message(data)
+		return []
 		
 class ActionInfo(Action):
 
-    def name(self):
-        return "action_service_info"
+	def name(self):
+		return "action_service_info"
 
-    def run(self, dispatcher, tracker, domain):
-        service = next(tracker.get_latest_entity_values('service'), None)
+	def run(self, dispatcher, tracker, domain):
+		service = next(tracker.get_latest_entity_values('service'), None)
 		searching_service = "none"
-        if service is not None:
-            searching_service = service
+		if service is not None:
+			searching_service = service
 		data = {
 			"intent" : "action_service_info",
 			"service" : searching_service
 		}
-        dispatcher.utter_message(data)
-        return []
+		dispatcher.utter_message(data)
+		return []
 
 class ActionUsingInfo(Action):
 
-    def name(self):
-        return "action_service_using_info"
+	def name(self):
+		return "action_service_using_info"
 
-    def run(self, dispatcher, tracker, domain):
-        service = next(tracker.get_latest_entity_values('service'), None)
+	def run(self, dispatcher, tracker, domain):
+		service = next(tracker.get_latest_entity_values('service'), None)
 		searching_service = "none"
-        if service is not None:
-            searching_service = service
+		if service is not None:
+			searching_service = service
 		data = {
 			"intent" : "action_service_using_info",
 			"service" : searching_service
 		}
-        dispatcher.utter_message(data)
-        return []
+		dispatcher.utter_message(data)
+		return []
 
 class ActionApiList(Action):
 
-    def name(self):
-        return "action_service_api_list"
+	def name(self):
+		return "action_service_api_list"
 
-    def run(self, dispatcher, tracker, domain):
-        service = next(tracker.get_latest_entity_values('service'), None)
+	def run(self, dispatcher, tracker, domain):
+		service = next(tracker.get_latest_entity_values('service'), None)
 		searching_service = "none"
-        if service is not None:
-            searching_service = service
+		if service is not None:
+			searching_service = service
 		data = {
 			"intent" : "action_service_api_list",
 			"service" : searching_service
 		}
-        dispatcher.utter_message(data)
-        return []
+		dispatcher.utter_message(data)
+		return []
