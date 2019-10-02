fb89715a56102f2408917474a39d43b241dfbb47
Jimting <handsome841206@gmail.com>
2019-10-02 17:54:29 +0800

    update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -1,3 +1,86 @@
 from rasa_core_sdk import Action
 
+class ActionHealth(Action):
 
+	def name(self):
+		return "action_service_health"
+
+	def run(self, dispatcher, tracker, domain):
+		service = next(tracker.get_latest_entity_values('service'), None)
+		searching_service = "none"
+		if service is not None:
+			searching_service = service
+		data = {
+			"intent" : "action_service_health",
+			"service" : searching_service
+		}
+		dispatcher.utter_message(format(data))
+		return []
+		
+class ActionInfo(Action):
+
+	def name(self):
+		return "action_service_info"
+
+	def run(self, dispatcher, tracker, domain):
+		service = next(tracker.get_latest_entity_values('service'), None)
+		searching_service = "none"
+		if service is not None:
+			searching_service = service
+		data = {
+			"intent" : "action_service_info",
+			"service" : searching_service
+		}
+		dispatcher.utter_message(format(data))
+		return []
+
+class ActionUsingInfo(Action):
+
+	def name(self):
+		return "action_service_using_info"
+
+	def run(self, dispatcher, tracker, domain):
+		service = next(tracker.get_latest_entity_values('service'), None)
+		searching_service = "none"
+		if service is not None:
+			searching_service = service
+		data = {
+			"intent" : "action_service_using_info",
+			"service" : searching_service
+		}
+		dispatcher.utter_message(format(data))
+		return []
+
+class ActionApiList(Action):
+
+	def name(self):
+		return "action_service_api_list"
+
+	def run(self, dispatcher, tracker, domain):
+		service = next(tracker.get_latest_entity_values('service'), None)
+		searching_service = "none"
+		if service is not None:
+			searching_service = service
+		data = {
+			"intent" : "action_service_api_list",
+			"service" : searching_service
+		}
+		dispatcher.utter_message(format(data))
+		return []
+
+class ActionServiceOnly(Action):
+
+	def name(self):
+		return "action_service_only"
+
+	def run(self, dispatcher, tracker, domain):
+		service = next(tracker.get_latest_entity_values('service'), None)
+		searching_service = "none"
+		if service is not None:
+			searching_service = service
+		data = {
+			"intent" : "action_service_only",
+			"service" : searching_service
+		}
+		dispatcher.utter_message(format(data))
+		return []
--- data/stories.md
+++ data/stories.md
@@ -1,21 +1,21 @@
 ## service_health
 * service_health
-  - utter_service_health
+  - action_service_health
   - action_restart
   
 ## service_info
 * service_info
-  - utter_service_info
+  - action_service_info
   - action_restart
   
 ## service_using_info
 * service_using_info
-  - utter_service_using_info
+  - action_service_using_info
   - action_restart
   
 ## service_api_list
 * service_api_list
-  - utter_service_api_list
+  - action_service_api_list
   - action_restart
   
 ## service_env
@@ -25,7 +25,7 @@
 
 ## service_only
 * service_only
-  - utter_service_only
+  - action_service_only
   - action_restart
   
 ## fallback story
